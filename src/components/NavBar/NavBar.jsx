00832f27dcb24b6cacde1cc8a79838b831bb44d1
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2023-03-30 22:39:24 +0100

    Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,49 +1,60 @@
-import React from 'react';
-import { images } from "../../constants";
-import { Link } from "react-router-dom";
+import React, { useState, useRef, useEffect } from 'react';
+import Notification from '../Notification/Notification';
+import navLogo from '../../assets/logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import "./NavBar.scss";
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (
+        navbarOpen &&
+        ref.current &&
+        !ref.current.contains(event.target)
+      ) {
+        setNavbarOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    }
+  }, [navbarOpen])
+  
+
   return (
-    <div className="app__navbar">
-        <div className="app__navbar-logo">
-            <img src={images.logo} alt="Logo" />
-            <p>LearnDev Foundation</p>
+    <div>
+      <Notification />
+
+      <div className="app__navbar" ref={ref}>
+        <div className="app__navbar_logo">
+          <img src={navLogo} alt="" />
+          <p>LearnDev Foundation</p>
         </div>
-        <div className="app__navbar-menu">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              Academy
-            </li>
-            <li>
-              Blog
-            </li>
-            <li>
-              About
-            </li>
+        <div className="app__navbar_hamburger">
+          <FontAwesomeIcon icon={faBars} beat className='.app__navbar_hamburger-icon' onClick={() => setNavbarOpen((prev) => !prev)}/>
+          <ul className={`hamburger ${navbarOpen ? 'show-hamburger' : ''}`}>
+            <li onClick={() => setNavbarOpen(false)}><a href="#">Home</a></li>
+            <li onClick={() => setNavbarOpen(false)}><a href="#">About</a></li>
+            <li onClick={() => setNavbarOpen(false)}><a href="#">Academy</a></li>
+            <li onClick={() => setNavbarOpen(false)}><a href="#">Blog</a></li>
           </ul>
         </div>
-        <div className="app__navbar-menu_line" />
-        <div className="app__navbar-social">
-          <a href="">
-            <img src={images.facebook} alt="" />
-          </a>
-          <a href="">
-            <img src={images.github} alt="" />
-          </a>
-          <a href="">
-            <img src={images.linkedin} alt="" />
-          </a>
-          <a href="">
-            <img src={images.twitter} alt="" />
-          </a>
+        <div className="app__navbar_menu">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Academy</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
         </div>
-        <div className="app__navbar-menu_line" />
-        
+      </div>
     </div>
   )
 }
